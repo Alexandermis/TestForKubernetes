98495213922df70c9759ce5e36bd42ab5a41b9bc
Alexandermis <73187089+Alexandermis@users.noreply.github.com>
2024-07-29 14:24:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    # Get some user data
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    # Print the user data to record it with filebeat
+    print(f"Client IP: {client_ip}")
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    # Start the testapp
     app.run(host='0.0.0.0', port=8080)
